765f06bfa8113d433c72c9ced67aa3f325220f5f
franklin rodriguez <franklinserif@gmail.com>
2024-08-02 09:10:30 -0400

    feat(interfaces): :label: add new Enum for  buttons and header style

---
+++ src/interfaces/themes.ts
@@ -0,0 +1,16 @@
+export enum ButtonVariant {
+    default = "default",
+    primary = "primary",
+    ghost = "ghost"
+}
+
+// Text
+export enum HeaderTextColor {
+    Primary = "Primary",
+    SecondaryWhite = "Secondary white",
+}
+
+export enum HeaderTextSize {
+    Normal = "Normal",
+    xl = "XL"
+}
