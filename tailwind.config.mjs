3e6f085b1d3a189e7908d853d04e9cca9fd4fdfc
franklin rodriguez <franklinserif@gmail.com>
2024-08-01 22:33:23 -0400

    ci(config/dependencies): :wrench: update tailwind theme configuration file
    add custom font fira code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"fire-code-normal": ["Fira Code"],
+				"fire-code-light": ["Fira Code light"],
+				"fire-code-medium": ["Fira Code medium"],
+				"fire-code-semi-bold": ["Fira Code semi-bold"],
+				"fire-code-bold": ["Fira Code bold"],
+				"fire-code-vf": ["Fira Code VF"],
+				firacode: {
+				}
+			}
+		},
 	},
 	plugins: [],
 }
