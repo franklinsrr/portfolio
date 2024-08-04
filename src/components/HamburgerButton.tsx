9e1d732be045e8b37412430628311909a74a7ee6
franklin rodriguez <franklinserif@gmail.com>
2024-08-04 18:50:01 -0400

    feat(components): :sparkles: add HamburgerButton

---
+++ src/components/HamburgerButton.tsx
@@ -0,0 +1,20 @@
+import { type FC } from "react";
+
+interface Props {
+    isOpen: boolean;
+    onClick: () => void;
+}
+
+const HamburgerButton: FC<Props> = ({ isOpen, onClick }) => {
+
+    return (
+        <button
+            className="h-[49px] border-r-[1px] border-linescolor px-6"
+            onClick={onClick}
+        >
+            <img src={isOpen ? "/close-icon.svg" : "/hamburger-icon.svg"} alt="hamburger" width="20px" height="20px" />
+        </button>
+    )
+}
+
+export default HamburgerButton;
