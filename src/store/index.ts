d262834ee344ca67fb86067220739de59a4d4668
franklin rodriguez <franklinserif@gmail.com>
2024-08-08 22:39:16 -0400

    feat(store): :sparkles: add useCategoryStore

---
+++ src/store/index.ts
@@ -0,0 +1,28 @@
+import { create } from "zustand";
+import { type Category } from "@interfaces/category";
+import { CATEGORIES } from "@constants/categories";
+
+type CategoryState = {
+    categories: Category[]
+}
+
+type CategoryAction = {
+    toggle: (Category: Category) => void;
+}
+
+export const useCategoryStore = create<CategoryState & CategoryAction>()((set) => ({
+    categories: CATEGORIES,
+    toggle: (category) => set((state) => {
+        const newCategories = state.categories?.map((cat) => {
+            if (cat?.title === category?.title) {
+                return category;
+            }
+
+            return cat;
+        })
+        return {
+            ...state,
+            categories: newCategories
+        }
+    })
+}))
