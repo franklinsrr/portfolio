1c118f21b3832a7731280c69b268992cc644e1b7
franklin rodriguez <franklinserif@gmail.com>
2024-08-08 22:45:42 -0400

    feat(components): :sparkles: add ProjectsCategory

---
+++ src/components/projects/ProjectsCategory.tsx
@@ -0,0 +1,28 @@
+import { type FC } from "react";
+import clsx from "clsx";
+import Checkbox from "@components/Checkbox";
+import type { Category } from "@interfaces/category";
+
+interface Props {
+    onCheck: (category: Category) => void;
+    category: Category;
+}
+
+const ProjectCategory: FC<Props> = ({ onCheck, category }) => {
+
+    const handleCheck = (isActive: boolean) => {
+        onCheck({ ...category, isActive })
+    }
+
+    return (
+        <div className="flex items-center font-fire-code-normal font-custom-big-size text-secondary-gray py-2 uppercase">
+            <Checkbox onCheck={handleCheck} />
+            <img src={category.src} alt={category.title} width="20px" height="20px" className={clsx("mx-4", { "opacity-40": !category.isActive })} />
+            <span>
+                {category.title}
+            </span>
+        </div>
+    )
+}
+
+export default ProjectCategory;
