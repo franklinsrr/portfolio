7b1e9ead71c5650374063b9a8a290d72cea7523b
franklin rodriguez <franklinserif@gmail.com>
2024-08-11 00:06:23 -0400

    feat(components): :sparkles: add AsideProject

--- src/components/projects/AsideProject.tsx
+++ src/components/projects/AsideProject.tsx
@@ -1,23 +1,46 @@
-import { type FC } from "react";
+import { type FC, useState } from "react";
 import ProjectCategory from "./ProjectsCategory";
 import { useCategoryStore } from "@store/index";
+import ToggleTitle from "@components/ToggleTitle";
 import type { Category } from "@interfaces/category";
 
+interface Props {
+    children: React.ReactNode;
+}
 
-const AsideProject: FC = () => {
+
+const AsideProject: FC<Props> = ({ children }) => {
     const categories = useCategoryStore((state) => state.categories);
     const toggleCategory = useCategoryStore(state => state.toggle);
-
+    const [isOpen, setIsOpen] = useState(true);
 
     const handleCategory = (Category: Category) => {
         toggleCategory(Category);
     }
 
+    const handleClick = () => {
+        setIsOpen((prev) => !prev);
+    }
+
+
     return (
-        <aside className="h-[calc(100vh-64px-100px)] flex flex-col items-start border-r-linescolor border-r-[1px] w-[310px] px-[25px] py-[10px]">
-            {categories?.map((cat) => (
-                <ProjectCategory category={cat} onCheck={handleCategory} key={cat.title} />
-            ))}
+        <aside className="h-[calc(100vh-64px-100px)] flex flex-col items-start border-r-linescolor border-r-[1px] lg:w-[310px] w-full">
+            <span className="h-[70px] font-fire-code-retina text-white justify-start items-center pl-6 flex lg:hidden">
+                _projects
+            </span>
+            <div className="w-full hidden lg:block">
+                {children}
+            </div>
+            <div className="w-full lg:hidden block">
+                <ToggleTitle isOpen={isOpen} onClick={handleClick}>
+                    projects
+                </ToggleTitle>
+            </div>
+            {isOpen && <div className="px-[25px] py-[10px]">
+                {categories?.map((cat) => (
+                    <ProjectCategory category={cat} onCheck={handleCategory} key={cat.title} />
+                ))}
+            </div>}
         </aside>
     )
 }
