c4f84038ffe1828ec8e96b62cfb08bd7cf60f455
franklin rodriguez <franklinserif@gmail.com>
2024-08-13 18:04:17 -0400

    chore(utils): :sparkles: add getThirdMenuTitle
    will return the tittle according to the pathname

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,6 @@
+
 import { thirdLevelLinks, ABOUT_SECTIONS } from "@constants/links";
+
 export function getMenuLinksByPathname(pathname: string) {
     const links = Object.entries(thirdLevelLinks);
 
@@ -13,4 +15,16 @@ export function getMenuLinksByPathname(pathname: string) {
     });
 
     return menuLinks?.[1];
+}
+
+export function getThirdMenuTitle(pathname: string) {
+    const menus = Object.entries(ABOUT_SECTIONS);
+
+    if (pathname === "/about" || pathname.includes("/about/professional-info")) {
+        return "professional-info";
+    }
+
+    const menu = menus.find((item) => pathname.includes(item[1].url));
+
+    return menu?.[1].title;
 }
