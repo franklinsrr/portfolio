9924f859555527cd1568ac4383c78e7ff3028df8
franklin rodriguez <franklinserif@gmail.com>
2024-08-13 18:10:12 -0400

    refactor(constants): :recycle: ABOUT_SECTIONS improvement
    add url and title for every link

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -8,27 +8,27 @@ import linkSVG from "@assets/link-icon.svg"
 import type { ArticleLink } from "@interfaces/links";
 
 export const ABOUT_SECTIONS = {
-    professionalInfo: "/about",
-    personalInfo: "/about/personal-info",
-    hobbies: "/about/hobbies"
+    professionalInfo: { url: "/about", title: "professional-info" },
+    personalInfo: { url: "/about/personal-info", title: "personal-info" },
+    hobbies: { url: "/about/hobbies", title: "hobbies-info" }
 }
 
 export const PROFESSIONAL_INFO_LINKS: ArticleLink[] = [
     {
         title: "experience",
-        href: `${ABOUT_SECTIONS.professionalInfo}`,
+        href: `${ABOUT_SECTIONS.professionalInfo.url}`,
         toggle: true,
         src: pinkFolderSVG.src
     },
     {
         title: "hard-skills",
-        href: `${ABOUT_SECTIONS.professionalInfo}/professional-info/hard-skills`,
+        href: `${ABOUT_SECTIONS.professionalInfo.url}/professional-info/hard-skills`,
         toggle: true,
         src: greenFolderSVG.src
     },
     {
         title: "soft-skills",
-        href: `${ABOUT_SECTIONS.professionalInfo}/professional-info/soft-skills`,
+        href: `${ABOUT_SECTIONS.professionalInfo.url}/professional-info/soft-skills`,
         toggle: true,
         src: purpleFolderSVG.src
     },
@@ -37,31 +37,31 @@ export const PROFESSIONAL_INFO_LINKS: ArticleLink[] = [
 export const PERSONAL_INFO_LINKS: ArticleLink[] = [
     {
         title: "bio",
-        href: `${ABOUT_SECTIONS.personalInfo}`,
+        href: `${ABOUT_SECTIONS.personalInfo.url}`,
         toggle: true,
         src: pinkFolderSVG.src
     },
     {
         title: "interests",
-        href: `${ABOUT_SECTIONS.personalInfo}/interests`,
+        href: `${ABOUT_SECTIONS.personalInfo.url}/interests`,
         toggle: true,
         src: greenFolderSVG.src
     },
     {
         title: "education",
-        href: `${ABOUT_SECTIONS.personalInfo}/education`,
+        href: `${ABOUT_SECTIONS.personalInfo.url}/education`,
         toggle: true,
         src: purpleFolderSVG.src
     },
     {
         title: "hight-school",
-        href: `${ABOUT_SECTIONS.personalInfo}/hight-school`,
+        href: `${ABOUT_SECTIONS.personalInfo.url}/hight-school`,
         toggle: false,
         src: degreeSVG.src
     },
     {
         title: "university",
-        href: `${ABOUT_SECTIONS.personalInfo}/university`,
+        href: `${ABOUT_SECTIONS.personalInfo.url}/university`,
         toggle: false,
         src: degreeSVG.src
     },
@@ -71,13 +71,13 @@ export const PERSONAL_INFO_LINKS: ArticleLink[] = [
 export const HOBBIES_LINKS: ArticleLink[] = [
     {
         title: "sports",
-        href: `${ABOUT_SECTIONS.hobbies}`,
+        href: `${ABOUT_SECTIONS.hobbies.url}`,
         toggle: true,
         src: pinkFolderSVG.src
     },
     {
         title: "favorite-games",
-        href: `${ABOUT_SECTIONS.hobbies}/favorites-games`,
+        href: `${ABOUT_SECTIONS.hobbies.url}/favorites-games`,
         toggle: true,
         src: greenFolderSVG.src
     },
@@ -112,3 +112,13 @@ export const SOCIAL_MEDIA_LINKS: ArticleLink[] = [
         src: linkSVG.src
     },
 ]
+
+interface IThirdLevelLinks {
+    [keyof: string]: ArticleLink[]
+}
+
+export const thirdLevelLinks: IThirdLevelLinks = {
+    "/about": PROFESSIONAL_INFO_LINKS,
+    "/about/personal-info": PERSONAL_INFO_LINKS,
+    "/about/hobbies": HOBBIES_LINKS,
+}
