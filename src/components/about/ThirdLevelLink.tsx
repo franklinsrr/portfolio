3effccb64300d835319eac65fe516f2d8a055bf0
franklin rodriguez <franklinserif@gmail.com>
2024-08-14 16:17:18 -0400

    refactor(components): :lipstick: ThirdLevelLink change font size to bigger

--- src/components/about/ThirdLevelLink.tsx
+++ src/components/about/ThirdLevelLink.tsx
@@ -13,7 +13,7 @@ const ThirdLevelLink: FC<Props> = ({ link, isActive }) => {
         <a href={link.href} className="w-full flex gap-3">
             {link.toggle && <img src={arrowGraySVG.src} alt="arrow" width="8px" height="8px" className={clsx(isActive && "rotate-90")} />}
             <img src={link.src} alt="folder icon" width="16px" height="21px" className={clsx({ "ml-5": !link.toggle })} />
-            <span className={clsx("font-custom-normal-size font-fire-code-normal text-secondary-gray hover:text-white", { "text-white": isActive })}>
+            <span className={clsx("font-custom-big-size font-fire-code-normal text-secondary-gray hover:text-white", { "text-white": isActive })}>
                 {link.title}
             </span>
         </a>
