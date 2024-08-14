45e5a3ee3ead033aef746852449ca91100dda31f
franklin rodriguez <franklinserif@gmail.com>
2024-08-14 19:12:15 -0400

    refactor(components): :recycle: ref ToggleTitle
    add new property tabBorder to toggle top border style

--- src/components/menu/ToggleTittle.tsx
+++ src/components/menu/ToggleTittle.tsx
@@ -1,15 +1,20 @@
 import { type FC } from "react";
 import downWhiteIconSVG from "@assets/down-white-arrow-icon.svg";
+import clsx from "clsx";
 
 
 interface Props {
     children: React.ReactNode;
     onClick: () => void;
+    topBorder?: boolean;
 }
 
-const ToggleTitle: FC<Props> = ({ children, onClick }) => {
+const ToggleTitle: FC<Props> = ({ children, onClick, topBorder = false }) => {
     return (
-        <button className="flex justify-start items-center h-8 lg:border-b-[1px] lg:border-b-linescolor" onClick={onClick}>
+        <button
+            className={clsx("flex justify-start items-center h-8 lg:border-b-[1px] lg:border-b-linescolor w-full",
+                { "border-t-linescolor border-t-[1px]": topBorder })}
+            onClick={onClick}>
             <img src={downWhiteIconSVG.src} alt="down arrow" height="7px" width="9px" className="mx-3" />
             <span className="font-fire-code-normal font-custom-normal-size text-white">
                 {children}
