3f202f2eed429539b7d12a4913ab726d8fa8de13
franklin rodriguez <franklinserif@gmail.com>
2024-08-15 19:46:58 -0400

    refactor(components): :recycle: LinkButton
    change from astro component to react

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,16 @@
+import { type FC } from "react"
+
+interface Props {
+    href: string;
+    children: React.ReactNode;
+}
+
+const LinkButton: FC<Props> = ({ href, children }) => {
+    return (
+        <a href={href} className="bg-button-default text-white hover:bg-buttonHover-default rounded-lg font-custom-size font-fire-code-normal px-3.5 py-2.5 transition-colors">
+            {children}
+        </a>
+    )
+}
+
+export default LinkButton;
