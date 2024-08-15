024c3bdadf926a0efac137fdfbde55d9b8bfe67c
franklin rodriguez <franklinserif@gmail.com>
2024-08-15 19:49:49 -0400

    feat(components): :sparkles: add ProjectHeader

---
+++ src/components/projects/ProjectHeader.tsx
@@ -0,0 +1,22 @@
+import { type FC } from "react";
+import PageHeader from "@components/PageHeader";
+import { useCategoryStore } from "@store/useCategoryStore";
+
+const ProjectHeader: FC = () => {
+    const cats = useCategoryStore((cats) => cats).categories.filter((cat) => cat.isActive).map((cat) => cat.title) || [];
+
+    let title = "all;"
+
+    if (cats.length > 0) {
+        title = cats.reduce((acc, currentValue) => {
+            acc += ` ${currentValue};`;
+            return acc;
+        },)
+    }
+
+    return (
+        <PageHeader title={title} />
+    )
+}
+
+export default ProjectHeader;
