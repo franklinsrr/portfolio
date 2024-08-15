d19714d07db796c354a84bcf50fe72c62c719967
franklin rodriguez <franklinserif@gmail.com>
2024-08-15 11:44:05 -0400

    feat(hooks): :sparkles: add useMobileDectector
    detech when the document is mobile size

---
+++ src/hooks/useMobileDectector.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+
+export const useMobileDetector = (size: number) => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const handleWindowSizeChange = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowSizeChange);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowSizeChange);
+        };
+    }, []);
+
+    const isMobile = width <= size;
+
+    return { isMobile, width };
+}
