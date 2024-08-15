58a759446598c8916f304a614dfb65e1409efdd5
franklin rodriguez <franklinserif@gmail.com>
2024-08-15 19:47:22 -0400

    refactor(components): :recycle: Technology
    change from astro component to react

---
+++ src/components/Technology.tsx
@@ -0,0 +1,16 @@
+import { type Tech } from '@interfaces/components'
+import { SVG_WITH_BG } from '@constants/svg';
+import type { FC } from 'react';
+
+interface Props {
+    tech: Tech;
+}
+
+const Technology: FC<Props> = ({ tech }) => {
+
+    const svgImage = SVG_WITH_BG[tech];
+    return <img src={svgImage.src} alt={tech} />
+}
+
+export default Technology;
+
