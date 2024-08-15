d3c64888b24f729e4ee3b849d7fd59311ab996bf
franklin rodriguez <franklinserif@gmail.com>
2024-08-15 11:37:57 -0400

    refactor(components): :recycle: ToggleTitle major improvement

--- src/components/menu/ToggleTittle.tsx
+++ src/components/menu/ToggleTittle.tsx
@@ -1,21 +1,28 @@
 import { type FC } from "react";
-import downWhiteIconSVG from "@assets/down-white-arrow-icon.svg";
 import clsx from "clsx";
-
+import downWhiteIconSVG from "@assets/down-white-arrow-icon.svg";
 
 interface Props {
     children: React.ReactNode;
     onClick: () => void;
     topBorder?: boolean;
+    isOpen?: boolean;
 }
 
-const ToggleTitle: FC<Props> = ({ children, onClick, topBorder = false }) => {
+const ToggleTitle: FC<Props> = ({ children, onClick, topBorder = false, isOpen = true }) => {
     return (
         <button
-            className={clsx("flex justify-start items-center h-8 lg:border-b-[1px] lg:border-b-linescolor w-full",
-                { "border-t-linescolor border-t-[1px]": topBorder })}
+            className={clsx("flex justify-start items-center h-8 lg:border-b-[1px] lg:border-b-linescolor w-full sm:bg-linescolor sm:pl-3 lg:bg-transparent",
+                { "border-t-linescolor border-t-[1px]": topBorder },
+            )}
             onClick={onClick}>
-            <img src={downWhiteIconSVG.src} alt="down arrow" height="7px" width="9px" className="mx-3" />
+            <img
+                src={downWhiteIconSVG.src}
+                alt="down arrow"
+                height="7px"
+                width="9px"
+                className={clsx("mx-3 sm:rotate-[-90deg]",
+                    { "sm:!rotate-[0deg]": isOpen })} />
             <span className="font-fire-code-normal font-custom-normal-size text-white">
                 {children}
             </span>
