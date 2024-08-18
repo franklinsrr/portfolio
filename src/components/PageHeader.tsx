d80037b842fd649383023abba846dee983483596
franklin rodriguez <franklinserif@gmail.com>
2024-08-18 14:48:33 -0400

    refactor(components): :lipstick: PageHeader - improve the responsive design

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -6,8 +6,11 @@ interface Props {
 
 const PageHeader: FC<Props> = ({ title }) => {
   return (
-    <div className="w-full border-b-[1px] border-b-linescolor h-[35px] font-custom-normal-size font-fire-code-normal text-secondary-gray flex items-center pl-4 py-0">
-      <span className="border-r-[1px] border-r-linescolor pr-6 h-[35px] flex items-center">
+    <div
+      className="w-full lg:border-b-[1px] lg:border-b-linescolor h-[35px] font-custom-normal-size font-fire-code-normal text-secondary-gray flex items-center pl-4 py-0">
+      <span className="flex lg:hidden text-white">// Projects&#160;</span>
+      <span className="flex lg:hidden text-secondary-gray">/&#160;</span>
+      <span className="lg:border-r-[1px] lg:border-r-linescolor pr-6 h-[35px] flex items-center">
         {title} x
       </span>
     </div>
