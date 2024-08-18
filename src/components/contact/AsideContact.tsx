9c5082356a3ba125de28cba4ba2fca71749e9e84
franklin rodriguez <franklinserif@gmail.com>
2024-08-18 19:53:02 -0400

    feat(components): :sparkles: add AsideContact

---
+++ src/components/contact/AsideContact.tsx
@@ -0,0 +1,34 @@
+import { type FC } from "react";
+import { useContactStore } from "@store/useContactStore";
+import CodeFormat from "@components/CodeFormat";
+import { formatDate } from "@utils/date";
+
+const AsideContact: FC = () => {
+    const form = useContactStore((state) => state.form);
+    const date = new Date().getTime();;
+    const formatted = formatDate(date);
+
+    const code = `const button = document.querySelector ( '#sendBtn' );
+const message = {
+   name : "${form.name}",
+   email : "${form.email}",
+   message : "${form.message}",
+   date:  "${formatted}"
+}
+
+
+button.addEventListener ( 'click' ), () => {
+  form.send ( message );
+})
+    `;
+
+    console.log("form: ", form.email);
+
+    return (
+        <div className="border-l-[1px] border-l-linescolor flex justify-center lg:py-28 py-10">
+            <CodeFormat code={code} fontSize={14} showLineNumber />
+        </div>
+    )
+}
+
+export default AsideContact;
