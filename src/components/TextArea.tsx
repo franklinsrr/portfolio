69f0abe00a2648b0a6caa24177058a7c1e95a6cf
franklin rodriguez <franklinserif@gmail.com>
2024-08-18 19:48:24 -0400

    feat(components): :sparkles: add TextArea

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,21 @@
+import type { TextareaHTMLAttributes, FC } from "react";
+
+interface Props {
+    onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+const TextArea: FC<Props & TextareaHTMLAttributes<HTMLTextAreaElement>> = ({ name, value, ...rest }) => {
+    return (
+        <fieldset className="flex flex-col gap-2 text-secondary-gray font-fire-code-retina font-custom-normal-size w-full">
+            <label htmlFor={name}>
+                _{name}
+            </label>
+            <textarea {...rest}
+                id={name}
+                className="border-2 border-linescolor bg-primary-dark rounded-md p-2 w-full overflow-hidden outline-none focus:border-secondary-gray focus:shadow-md focus:shadow-secondary-gray/40"
+            />
+        </fieldset>
+    )
+}
+
+export default TextArea;
