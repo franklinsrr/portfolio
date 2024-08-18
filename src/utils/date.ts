5b81877b707621e434877733c02b32868a66a677
franklin rodriguez <franklinserif@gmail.com>
2024-08-18 19:48:48 -0400

    feat(utils): :sparkles: add formatDate function

---
+++ src/utils/date.ts
@@ -0,0 +1,10 @@
+export function formatDate(date: Date | number) {
+    const formatter = new Intl.DateTimeFormat('en-us', {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+    })
+
+    return formatter.format(date);
+}
