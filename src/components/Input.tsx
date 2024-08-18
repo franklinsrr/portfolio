b651fbc38657a9362f0aa63f9a856123e421a33b
franklin rodriguez <franklinserif@gmail.com>
2024-08-18 19:48:09 -0400

    feat(components): :sparkles: add Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,22 @@
+import type { InputHTMLAttributes, FC } from "react";
+
+type Props = InputHTMLAttributes<HTMLInputElement> &
+{ onChange: (event: React.ChangeEvent<HTMLInputElement>) => void }
+
+const Input: FC<Props> = ({ name, onChange, ...rest }) => {
+    return (
+        <fieldset className="flex flex-col gap-2 text-secondary-gray font-fire-code-retina font-custom-normal-size w-full">
+            <label htmlFor={name}>
+                _{name}
+            </label>
+            <input
+                onChange={onChange}
+                className="border-2 border-linescolor bg-primary-dark rounded-md h-[40px] p-2 w-full outline-none focus:border-secondary-gray focus:shadow-md focus:shadow-secondary-gray/40"
+                id={name}
+                {...rest}
+            />
+        </fieldset>
+    )
+}
+
+export default Input;
