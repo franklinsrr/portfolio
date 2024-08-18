c0f5f9e0bbdb178b7a619eed5a02d58a4e15121e
franklin rodriguez <franklinserif@gmail.com>
2024-08-18 19:47:56 -0400

    feat(components): :sparkles: add ButtonIn

---
+++ src/components/ButtonInt.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "react";
+
+interface Props {
+    children: React.ReactNode;
+    onClick: () => void;
+}
+
+const ButtonIn: FC<Props> = ({ onClick, children }) => {
+    return (
+        <button
+            className="rounded-lg font-custom-size font-fire-code-normal px-3.5 py-2.5 transition-colors bg-button-default text-white hover:bg-buttonHover-default w-fit"
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default ButtonIn;
