d3c80ea8f20fbd18e3647e55533639ff743fa786
franklin rodriguez <franklinserif@gmail.com>
2024-08-21 16:38:56 -0400

    refactor(components): :recycle: ButtonIn - improve logic

--- src/components/ButtonInt.tsx
+++ src/components/ButtonInt.tsx
@@ -1,19 +1,22 @@
-import type { ButtonHTMLAttributes, FC } from "react";
+import clsx from "clsx";
+import type { AnchorHTMLAttributes, FC } from "react";
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+type Props = AnchorHTMLAttributes<HTMLAnchorElement> & {
   children: React.ReactNode;
+  isActive: boolean;
 };
 
-const ButtonIn: FC<Props> = ({ children, ...rest }) => {
-  const handleClick = () => {};
-
+const ButtonIn: FC<Props> = ({ children, href, isActive, ...rest }) => {
   return (
-    <button
-      className="rounded-lg font-custom-size font-fire-code-normal px-3.5 py-2.5 transition-colors bg-button-default text-white hover:bg-buttonHover-default w-fit"
+    <a
+      href={isActive ? href : "javascript:void(0)"}
+      className={clsx("rounded-lg font-custom-size font-fire-code-normal px-3.5 py-2.5 transition-colors bg-button-default text-white hover:bg-buttonHover-default w-fit", {
+        "bg-secondary-gray": !isActive
+      })}
       {...rest}
     >
       {children}
-    </button>
+    </a>
   );
 };
 
