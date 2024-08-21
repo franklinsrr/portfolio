ca8bf25a3474e1ae61cae78ab0cad7fcafade493
franklin rodriguez <franklinserif@gmail.com>
2024-08-21 16:39:29 -0400

    feat(components): :sparkles: add Sended

---
+++ src/components/Sended.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+const Sended: FC = () => {
+
+    return (
+        <div className="w-full h-full flex justify-center items-center py-96 text-secondary-gray text-4xl font-fire-code-retina">
+            email sent 🚀!!!
+        </div>
+    )
+}
+
+export default Sended;
