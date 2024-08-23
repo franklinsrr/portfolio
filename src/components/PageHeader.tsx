74579a0ab25f8f2bfcd157a045575af3f1d58c4c
franklin rodriguez <franklinserif@gmail.com>
2024-08-22 21:19:38 -0400

    refactor(components): :recycle: PageHeader - improve the component functionality

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,16 +1,17 @@
+import { getThirdMenuTitle } from "@utils/url";
 import { type FC } from "react";
 
 interface Props {
   title: string;
-  hideProjectTitle?: boolean;
+  pathname: string;
 }
 
-const PageHeader: FC<Props> = ({ title, hideProjectTitle = false }) => {
+const PageHeader: FC<Props> = ({ title, pathname }) => {
+  let pageTitle = getThirdMenuTitle(pathname);
+
   return (
     <div className="w-full lg:border-b-[1px] lg:border-b-linescolor h-[35px] font-custom-normal-size font-fire-code-normal text-secondary-gray flex items-center pl-4 py-0">
-      {!hideProjectTitle && (
-        <span className="flex lg:hidden text-white">// Projects&#160;</span>
-      )}
+      <span className="flex lg:hidden text-white">// {pageTitle}&#160;</span>
       <span className="flex lg:hidden text-secondary-gray">/&#160;</span>
       <span className="lg:border-r-[1px] lg:border-r-linescolor pr-6 h-[35px] flex items-center">
         {title} x
