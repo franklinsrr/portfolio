7c3cf6bd072d60901e111f5dc2d3b26d6f2d9490
franklin rodriguez <franklinserif@gmail.com>
2024-08-23 17:22:46 -0400

    refactor(components): :recycle: PageHeader add new getThirdMenuTitle

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,19 +2,19 @@ import { getThirdMenuTitle } from "@utils/url";
 import { type FC } from "react";
 
 interface Props {
-  title: string;
+  title?: string;
   pathname: string;
 }
 
 const PageHeader: FC<Props> = ({ title, pathname }) => {
-  const pageTitle = getThirdMenuTitle(pathname);
+  const [pageTitle, subPageTitle ]= getThirdMenuTitle(pathname);
 
   return (
     <div className="w-full lg:border-b-[1px] lg:border-b-linescolor h-[35px] font-custom-normal-size font-fire-code-normal text-secondary-gray flex items-center pl-4 py-0">
       <span className="flex lg:hidden text-white">// {pageTitle}&#160;</span>
       <span className="flex lg:hidden text-secondary-gray">/&#160;</span>
       <span className="lg:border-r-[1px] lg:border-r-linescolor pr-6 h-[35px] flex items-center">
-        {title} x
+        {title || subPageTitle} x
       </span>
     </div>
   );
