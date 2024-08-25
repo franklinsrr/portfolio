82f3ba49a2f67c864b7f4946cd457775f7a57e98
franklin rodriguez <franklinserif@gmail.com>
2024-08-24 21:36:59 -0400

    feat(components): :sparkles: add FoodScore and ShowFoodLeft components

---
+++ src/components/snake/FoodScore.tsx
@@ -0,0 +1,14 @@
+import {type FC } from "react";
+import "@components/snake/showFoodLeft.css";
+import clsx from "clsx";
+
+
+interface Props{
+    isActive?: boolean
+}
+
+const FoodScore:FC<Props> = ({isActive}) => {
+    return <div className={clsx("food", {"!opacity-100": isActive})}></div>
+}
+
+export default FoodScore;
