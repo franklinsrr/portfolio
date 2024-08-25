60389e9bfd1e7e90a43668ced734ad32ca17642e
franklin rodriguez <franklinserif@gmail.com>
2024-08-24 21:38:34 -0400

    refactor(components): :art: format code

--- src/components/snake/FoodScore.tsx
+++ src/components/snake/FoodScore.tsx
@@ -1,14 +1,13 @@
-import {type FC } from "react";
+import { type FC } from "react";
 import "@components/snake/showFoodLeft.css";
 import clsx from "clsx";
 
-
-interface Props{
-    isActive?: boolean
+interface Props {
+  isActive?: boolean;
 }
 
-const FoodScore:FC<Props> = ({isActive}) => {
-    return <div className={clsx("food", {"!opacity-100": isActive})}></div>
-}
+const FoodScore: FC<Props> = ({ isActive }) => {
+  return <div className={clsx("food", { "!opacity-100": isActive })}></div>;
+};
 
 export default FoodScore;
--- src/components/snake/ShowFoodLeft.tsx
+++ src/components/snake/ShowFoodLeft.tsx
@@ -1,19 +1,21 @@
-import {type FC } from "react";
+import { type FC } from "react";
 import FoodScore from "@components/snake/FoodScore";
 
-const ShowFoodLeft:FC = () => {
-    return <div className="grid grid-cols-5 gap-5 justify-items-center pt-5 w-fit">
-       <FoodScore isActive/>
-       <FoodScore/>
-       <FoodScore/>
-       <FoodScore/>
-       <FoodScore/>
-       <FoodScore/>
-       <FoodScore/>
-       <FoodScore/>
-       <FoodScore/>
-       <FoodScore/>
+const ShowFoodLeft: FC = () => {
+  return (
+    <div className="grid grid-cols-5 gap-5 justify-items-center pt-5 w-fit">
+      <FoodScore isActive />
+      <FoodScore />
+      <FoodScore />
+      <FoodScore />
+      <FoodScore />
+      <FoodScore />
+      <FoodScore />
+      <FoodScore />
+      <FoodScore />
+      <FoodScore />
     </div>
-}
+  );
+};
 
 export default ShowFoodLeft;
